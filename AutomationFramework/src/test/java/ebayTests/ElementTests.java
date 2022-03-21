2522fa93dd04c1beb81acec1eac41c05ab373cda
Corey Stacy <stacy.corey1992@gmail.com>
2022-03-21 16:06:55 -0400

    organizing

--- AutomationFramework/src/test/java/ebayTests/ElementTests.java
+++ AutomationFramework/src/test/java/ebayTests/ElementTests.java
@@ -9,38 +9,38 @@ public class ElementTests {
 	HomePage hp = new HomePage();
 
 	
-	@Test
-	public void expandMenus() {
-		hp.driverWait(10);
-		hp.motorsAndFashion();
-		hp.fashionAndElectronics();
-		hp.electronicsAndCollectibles();
-		hp.HnGAndCollectibles();
-		hp.HnGAndSportingGoods();
-		hp.toysAndBusiness();
-		hp.businessAndMusic();
-		hp.musicAndDeals();
-		
-	}
-	
-	@Test
-	public void registerTest() {
-		// placeholder values for registration info
-		//int required for password to pass
-		hp.driverWait(15);
-		int passNum = 52;
-		hp.register("First", "Last", "Email", "Password" + passNum);
-	}
-	
-	@Test
-	public void signInTest() {
-		// placeholder values
-		hp.signIn("User", "Password");
-	}
-	
-	@Test
-	public void SpotlightTest() {
-		hp.clickSpotlight();
-	}
-	
+//	@Test
+//	public void expandMenus() {
+//		hp.driverWait(10);
+//		hp.motorsAndFashion();
+//		hp.fashionAndElectronics();
+//		hp.electronicsAndCollectibles();
+//		hp.HnGAndCollectibles();
+//		hp.HnGAndSportingGoods();
+//		hp.toysAndBusiness();
+//		hp.businessAndMusic();
+//		hp.musicAndDeals();
+//		
+//	}
+//	
+//	@Test
+//	public void registerTest() {
+//		// placeholder values for registration info
+//		//int required for password to pass
+//		hp.driverWait(15);
+//		int passNum = 52;
+//		hp.register("First", "Last", "Email", "Password" + passNum);
+//	}
+//	
+//	@Test
+//	public void signInTest() {
+//		// placeholder values
+//		hp.signIn("User", "Password");
+//	}
+//	
+//	@Test
+//	public void SpotlightTest() {
+//		hp.clickSpotlight();
+//	}
+//	
 }
